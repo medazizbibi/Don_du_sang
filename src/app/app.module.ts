5c1c0860fad68fc82354f5d5acefafb413c57756
Mohamed Aziz Bibi <Medazizbibi@outlook.fr>
2019-04-17 11:04:42 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -10,8 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MDBBootstrapModule.forRoot(),
     AppRoutingModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
